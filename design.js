df803d4b6bdc0b294f97ce8cab6b5e973ede4040
Deraz <34086530+Deraz@users.noreply.github.com>
2019-10-19 01:59:02 +0200

    Adding Comments

--- design.js
+++ design.js
@@ -1,15 +1,22 @@
+// Fetching the submit button and the canvas container
 var submit = document.getElementById("button");
 var container = document.getElementById("cont");
 
-
+//Start makeGrid Fn on click
 submit.addEventListener("click",() => {
+  //Fetching the input fields values
   var height = document.getElementById("inputHeight").value;
   var width = document.getElementById("inputWidth").value;
+  
+  //removing all container elements (Refreshing)
   container.innerHTML='';
   container.style.backgroundColor= "white";
+  
+  //layout using CSS3 grid
   container.style.gridTemplateColumns =  `repeat(${width}, 1fr)`;
   container.style.gridTemplateRows =  `repeat(${height}, 1fr)`;
 
+  //Appending divs (cells) as children into the container 
   for (i=0; i<height*width; i++)
   {
     var cell = document.createElement("div");
@@ -18,11 +25,13 @@ submit.addEventListener("click",() => {
     container.appendChild(cell);
   }
 });
+//End makeGrid Fn on click
 
-
-
+//Fetching the cell click event to get the exact id of the cell clicked
 container.addEventListener("click" , (e) => {
   var thisCell = document.getElementById(e.target.id);
+  //Fetching the color value
   var color = document.getElementById("colorPicker").value;
+  //giving CSS background color for the cell clicked using the color value in the color picker
   thisCell.style.backgroundColor = color;
 });
